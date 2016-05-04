89a8d1f1c634763d5583d7a608b9b8a1770d3baa
Junpeng Qiu <qjpchmail@gmail.com>
2016-05-04 03:22:50 -0400

    Add docs

--- pinyinlib.el
+++ pinyinlib.el
@@ -20,6 +20,100 @@
 
 ;;; Commentary:
 
+;;                              ______________
+
+;;                               PINYINLIB.EL
+
+;;                               Junpeng Qiu
+;;                              ______________
+
+
+;; Table of Contents
+;; _________________
+
+;; 1 Functions
+;; .. 1.1 `pinyinlib-build-regexp-char'
+;; .. 1.2 `pinyinlib-build-regexp-string'
+;; 2 Packages that Use This Library
+;; 3 Contribute
+
+
+;; Library for converting first letter of Pinyin to Chinese characters.
+
+
+;; 1 Functions
+;; ===========
+
+;; 1.1 `pinyinlib-build-regexp-char'
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+;;   `pinyinlib-build-regexp-char' converts a letter to a regular
+;;   expression containing all the Chinese characters whose pinyins start
+;;   with the letter. It accepts four parameters:
+;;   ,----
+;;   | char &optional no-punc-p tranditional-p only-chinese-p
+;;   `----
+
+;;   The first parameter `char' is the letter to be converted. The latter
+;;   three parameters are optional.
+;;   - If `no-punc-p' is `t': it will not convert English punctuations to
+;;     Chinese punctuations.
+
+;;   - If `traditional-p' is `t': traditional Chinese characters are used
+;;     instead of simplified Chinese characters.
+
+;;   - If `only-chinese-p' is `t': the resulting regular expression doesn't
+;;     contain the English letter `char'.
+
+;;   When converting English punctuactions to Chinese punctuations, it used
+;;   the following table:
+;;    English Punctuation  Chinese & English Punctuations
+;;   -----------------------------------------------------
+;;    .                    。.
+;;    ,                    ，,
+;;    ?                    ？?
+;;    :                    ：:
+;;    !                    ！!
+;;    ;                    ；;
+;;    \\                   、\\
+;;    (                    （(
+;;    )                    ）)
+;;    <                    《<
+;;    >                    》>
+;;    ~                    ～~
+;;    '                    ‘’「」'
+;;    "                    “”『』\"
+;;    *                    ×*
+;;    $                    ￥$
+
+
+;; 1.2 `pinyinlib-build-regexp-string'
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+;;   It is same as `pinyinlib-build-regexp-char', except that its first
+;;   parameter is a string so that it can convert a sequence of letters to
+;;   a regular expression.
+
+
+;; 2 Packages that Use This Library
+;; ================================
+
+;;   - [ace-pinyin]
+;;   - [evil-find-char-pinyin]
+
+
+;; [ace-pinyin] https://github.com/cute-jumper/ace-pinyin
+
+;; [evil-find-char-pinyin]
+;; https://github.com/cute-jumper/evil-find-char-pinyin
+
+
+;; 3 Contribute
+;; ============
+
+;;   Contributions are always welcome. If you want to add some common
+;;   pinyin related functions that might be useful for other packages,
+;;   please send me a PR.
 ;;
 
 ;;; Code:
